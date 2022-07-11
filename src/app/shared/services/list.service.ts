9fcf0b9a70c562888278b4a3e6db649097ed8e22
Jean Silva <jeanpsilva10@gmail.com>
2022-07-11 18:49:51 -0300

    Adicionando arquivos do projeto

---
+++ src/app/shared/services/list.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+import { Observable } from 'rxjs';
+
+export type ApiResponse= {
+  info:{},
+  results:{},
+}
+@Injectable({
+  providedIn: 'root'
+})
+export class ListService {
+  page: number=1;
+  apiURL = `https://rickandmortyapi.com/api/character/?page=${this.page}`;
+
+  constructor(private httpClient: HttpClient) {  }
+
+  getList():Observable<any> {
+    return this.httpClient.get<any>(this.apiURL)
+  }
+  
+}
+
+
